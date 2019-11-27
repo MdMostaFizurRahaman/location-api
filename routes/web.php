bd0602113a31edbb61d1f5519232e527b108d295
=mostafizur@bansberry.com <=mostafizur@bansberry.com>
2019-11-27 14:58:49 +0600

    Request Logger added

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 
-Route::group(['middleware' =>['insert_response']], function(){
+Route::group(['middleware' =>['request_logger']], function(){
     Route::get('/getLocation','LocationController@getLocation');
 });
 
